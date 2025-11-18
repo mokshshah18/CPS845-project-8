e2ac4635c8b34a404c234d2696d3021e8dd2999c
kelsip <kelsipatel17@gmail.com>
2025-11-17 20:27:57 -0500

    all qr codes and campus

--- frontend/src/CampusNavigator.tsx
+++ frontend/src/CampusNavigator.tsx
@@ -345,6 +345,28 @@ const CampusNavigator: React.FC = () => {
                         gap: "10px",
                     }}
                 >
+                    {/* NEW BUTTONS ADDED HERE */}
+                    <button
+                        className="top-btn"
+                        onClick={() =>
+                            window.open("https://www.torontomu.ca/maps/", "_blank")
+                        }
+                    >
+                        Campus
+                    </button>
+
+                    <button
+                        className="top-btn"
+                        onClick={() =>
+                            window.open(
+                                "https://docs.google.com/document/d/1VhYMiE7ArwvtIqxLMAjNsqH8Y_maiJhwtpNFHs5WgIs/edit?usp=sharing",
+                                "_blank"
+                            )
+                        }
+                    >
+                        QR Codes
+                    </button>
+
                     <button
                         onClick={() => setShowDebug(true)}
                         className="debug-btn"
